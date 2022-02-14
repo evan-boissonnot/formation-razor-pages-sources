a2296dac271076257bda02d44ac6207697ef21bf
Evan BOISSONNOT <evan@boissonnot.fr>
2022-02-14 12:10:58 +0100

    refacto: Ajout d'un dossier par quête (depuis la 4)

---
+++ suivi-des-drones-quete-005/suivi-des-drones.Core.Models/Drone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suivi_des_drones.Core.Models
+{
+    /// <summary>
+    /// Drone de l'application
+    /// </summary>
+    public class Drone
+    {
+        #region Properties
+        public string Matricule { get; set; } = string.Empty;
+
+        public DateTime CreationDate { get; set; }
+
+        // public HealthStatus HealthStatus { get; set; } = HealthStatus.OK;
+
+        public HealthStatus HealthStatus { get; set; } =  HealthStatus.OK;
+        #endregion
+    }
+}
