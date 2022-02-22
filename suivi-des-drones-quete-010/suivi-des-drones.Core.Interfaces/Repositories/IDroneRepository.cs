d12ca1560f124c22a13e0e2e2fe95ff419df9b7b
Evan BOISSONNOT <evan@boissonnot.fr>
2022-02-22 11:36:29 +0100

    fin quete 10

---
+++ suivi-des-drones-quete-010/suivi-des-drones.Core.Interfaces/Repositories/IDroneRepository.cs
@@ -0,0 +1,17 @@
+﻿using suivi_des_drones.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suivi_des_drones.Core.Interfaces.Repositories
+{
+    /// <summary>
+    /// Contrat pour tout repository gérant des drones
+    /// </summary>
+    public interface IDroneRepository
+    {
+        List<Drone> GetAll();
+    }
+}
