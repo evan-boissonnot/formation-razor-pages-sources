8f645bd63f8a0f15e8f9c20db680d27cae38575f
Evan BOISSONNOT <evan@boissonnot.fr>
2022-03-04 10:38:22 +0100

    debut quete 15

---
+++ suivi-des-drones-quete-015/suivi-des-drones.Core.Infrastructure.Web/Middlewares/RedirectIsNotConnectedMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suivi_des_drones.Core.Infrastructure.Web.Middlewares
+{
+    public class RedirectIsNotConnectedMiddleware
+    {
+        #region Fields
+        private readonly RequestDelegate next;
+        #endregion
+
+        #region Constructors
+        public RedirectIsNotConnectedMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+        #endregion
+
+        #region Public methods
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var id = context.Session.GetString("UserId");
+            var isLoginPage = context.Request.Path.Value?.ToLower().Contains("login");
+
+            if (string.IsNullOrEmpty(id) && (!isLoginPage.HasValue || !isLoginPage.Value))
+            {
+                context.Response.Redirect("/login");
+                return;
+            }
+
+            await this.next.Invoke(context);
+        }
+        #endregion
+    }
+
+    public static class AuthenticationMiddleWares
+    {
+        #region Public methods
+        public static IApplicationBuilder UseRedirectIfNotConnected(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<RedirectIsNotConnectedMiddleware>();
+        }
+        #endregion
+    }
+}
---
+++ suivi-des-drones-quete-015/suivi-des-drones.Core.Infrastructure/DataLayers/SqlServerDroneDataLayer.cs
@@ -0,0 +1,40 @@
+﻿using suivi_des_drones.Core.Infrastructure.Databases;
+using suivi_des_drones.Core.Interfaces.Infrastructures;
+using suivi_des_drones.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace suivi_des_drones.Core.Infrastructure.DataLayers
+{
+    public class SqlServerDroneDataLayer : BaseSqlServerDataLayer, IDroneDataLayer
+    {
+        #region Constructors
+        public SqlServerDroneDataLayer(DronesDbContext context) : base(context) {}
+        #endregion
+
+        #region Public methods
+        public List<Drone> GetList()
+        {
+            var query = from item in this.Context?.Drones.Include(item => item.HealthStatus)
+                        //where item.CreationDate > DateTime.Now
+                        select item;
+
+            return query.ToList();
+        }
+
+        public void AddOne(Drone drone)
+        {
+            this.Context?.Add(drone);
+
+            //var entry = this.context?.Entry(drone.HealthStatus);
+            //entry.State = Microsoft.EntityFrameworkCore.EntityState.Unchanged;
+
+            this.Context?.SaveChanges();
+        }
+        #endregion
+    }
+}
